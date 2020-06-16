dacb6e52d5784ac120b5c6aabe57b3e1dbed722d
Sean <onslowsean@gmail.com>
2020-06-16 21:02:34 +0100

    Initial README

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $rules = 0;
 
 echo "Rules are: \n 1. match suit only\n 2. match the face of the card\n 3. match both suit and face of card\n";
 
-while(intval($rules) == 0){
+while(intval($rules) <= 0 || intval($rules) > 3){
 
 	$rules = readline("Enter number of rules: ");
 
